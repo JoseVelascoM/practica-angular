d85bde7e3833701a34deac00ef6026e5399048f7
JVMTBB <josepe_789@hotmail.com>
2017-10-17 19:11:41 -0400

    Angular material intalled

---
+++ src/app/material/material.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MATERIAL_COMPATIBILITY_MODE, MatButtonModule, MatIconModule, MatToolbarModule } from '@angular/material';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    MatButtonModule,
+    MatIconModule,
+    MatToolbarModule,
+    BrowserAnimationsModule
+  ],
+  exports: [
+    MatButtonModule,
+    MatIconModule,
+    MatToolbarModule
+  ],
+  providers: [
+    { provide: MATERIAL_COMPATIBILITY_MODE, useValue: true }
+  ],
+  declarations: []
+})
+export class MaterialModule { }
